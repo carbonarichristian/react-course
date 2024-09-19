67d4c940b0d18be0c3a9e5240b63b82c54476608
christian carbonari <christiancarbonari9@gmail.com>
2024-09-19 10:34:06 +1000

    travel-list building form

--- ultimate-react-course-main/05-travel-list/travel-list-app/src/App.js
+++ ultimate-react-course-main/05-travel-list/travel-list-app/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <h1>🌴Far Away⛴️</h1>
+  )
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your trip?</h3>
+      <select>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map ((num) => (
+          <option key={num} value={num}>{num}</option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..."></input>
+      <button>Add</button>
+    </form>
+  )
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <h2>Packing List</h2>
+      <ul>
+        {initialItems.map(item => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function Item(props) {
+  return (
+    <li>
+      <span style={props.item.packed ? {textDecoration: "line-through"} : null}>
+        {props.item.quantity} {props.item.description}
+      </span>
+      <button>❌❌</button>
+    </li>
+  )
+
+}
+
+function Stats() {
+  return (
+    <div className="stats">
+      <h2>Stats</h2>
+      <p>Items packed: 3</p>
+    </div>
+  )
+}
